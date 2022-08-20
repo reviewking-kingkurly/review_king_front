e742387801afc8301f7cc188314f66ad33e06987
DevSeulgi <dev.dinobox@gmail.com>
2022-08-21 01:22:22 +0900

    ADD: styled 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@emotion/react';
+import theme from './styles/theme';
 import App from './App';
+import GlobalStyle from '../styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle>
+        <App />
+      </GlobalStyle>
+    </ThemeProvider>
   </React.StrictMode>
 );
