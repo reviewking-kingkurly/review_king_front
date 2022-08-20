efa4f820e29381570b7335c7732f1f014d438473
DevSeulgi <dev.dinobox@gmail.com>
2022-08-21 01:43:48 +0900

    ADD: styles 폴더

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@emotion/react';
 import theme from './styles/theme';
 import App from './App';
-import GlobalStyle from '../styles/GlobalStyle';
+import GlobalStyle from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyle>
-        <App />
-      </GlobalStyle>
+      <GlobalStyle />
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
