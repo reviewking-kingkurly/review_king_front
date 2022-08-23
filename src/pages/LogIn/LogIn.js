6d5e79e11ea6218c2e067f2687e62fac86be5a53
DevSeulgi <dev.dinobox@gmail.com>
2022-08-24 07:12:48 +0900

    ADD: login message

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,79 +1,82 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { styled } from '@mui/material/styles';
+import styled from '@emotion/styled';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { IP } from '../../config';
-
-const theme = createTheme();
 
 const LogIn = () => {
   const navigate = useNavigate();
   const handleSubmit = async event => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-
     const userData = {
       user_email: data.get('email'),
       user_password: data.get('password'),
     };
-
     try {
       const res = await axios.post(`${IP}users/login`, userData);
-
       if (res.statusText === 'OK') {
-        alert(res.data.message);
         localStorage.setItem('access_token', res.data.access_token);
         navigate('/');
       }
     } catch (error) {
-      alert(error);
+      alert('아이디와 비밀번호를 확인해주세요');
     }
   };
 
   return (
-    <LoginContainer component="main" maxWidth="xs">
-      <CssBaseline />
-      <LoginBox>
-        <LogoWrapper>
-          <Logo src="/Logo.png" />
-        </LogoWrapper>
-        <LoginForm component="form" onSubmit={handleSubmit} noValidate>
-          <IdInput
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="아이디"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <PwInput
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="비밀번호"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <LoginButton type="submit" fullWidth variant="contained">
-            로그인
-          </LoginButton>
-        </LoginForm>
-      </LoginBox>
-    </LoginContainer>
+    <BackGround>
+      <LoginContainer component="main" maxWidth="xs">
+        <CssBaseline />
+        <LoginBox>
+          <LogoWrapper>
+            <Logo src="/Logo.png" />
+          </LogoWrapper>
+          <LoginForm component="form" onSubmit={handleSubmit} noValidate>
+            <IdInput
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="아이디를 입력해주세요"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <PwInput
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="비밀번호를 입력해주세요"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <LoginButton type="submit" fullWidth variant="contained">
+              로그인
+            </LoginButton>
+          </LoginForm>
+        </LoginBox>
+      </LoginContainer>
+    </BackGround>
   );
 };
 
 export default LogIn;
+
+const BackGround = styled.div`
+  ${({ theme }) => theme.flex.flexBox}
+  background-color: #FCFAFF;
+  opacity: 90%;
+  width: 100vw;
+  height: 100vh;
+`;
 
 const LoginContainer = styled(Container)`
   display: flex;
@@ -86,7 +89,6 @@ const LoginBox = styled(Box)`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 7rem;
   padding: 3rem 3rem;
   border: 1px solid #eee;
   border-radius: 5px;
@@ -110,7 +112,7 @@ const IdInput = styled(TextField)`
   margin-top: 3.25rem;
 `;
 const PwInput = styled(TextField)`
-  margin: 0;
+  margin: 0.2rem 0 0 0;
 `;
 
 const LoginButton = styled(Button)`
@@ -125,6 +127,6 @@ const LoginButton = styled(Button)`
   &:hover {
     color: #ffffff;
     background-color: #5e0080;
-    box-shadow: none;s
+    box-shadow: none;
   }
 `;
