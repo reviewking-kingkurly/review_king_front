2b7c16ca33b3cbc2041eea058939a96183710953
DevSeulgi <dev.dinobox@gmail.com>
2022-08-23 15:57:09 +0900

    FIX: conflict

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Routes, Route } from 'react-router-dom';
 import LogIn from './LogIn/LogIn';
 import Main from './Main/Main';
 import NavBar from './NavBar/NavBar';
 import ProductDetail from './ProductDetail/ProductDetail';
-=======
 import ReviewWrite from './pages/Review/ReviewWrite';
 import ReviewPost from './pages/Review/ReviewPost';
 import Search from './pages/Search/Search';
->>>>>>> origin
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
-<<<<<<< HEAD
         <Route path="/login" element={<LogIn />} />
         <Route path="/" element={<Main />} />
         <Route path="/products" element={<ProductDetail />} />{' '}
         {/*<Route path="/products/:id" element={<ProductDetail />} /> */}
-=======
         <Route path="/review_write" element={<ReviewWrite />} />
         <Route path="/review_post" element={<ReviewPost />} />
         <Route path="/search" element={<Search />} />
->>>>>>> origin
       </Routes>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom';
-=======
 import { ThemeProvider } from '@emotion/react';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
->>>>>>> origin
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
