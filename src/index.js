8ce94a69a473c393bc8ece49117fdf6bb7d9bd20
Seulgi Park <97112697+DevSeulgi@users.noreply.github.com>
2022-08-23 17:19:40 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  ThemeProvider as ThemeProviderMui,
+  createTheme,
+} from '@mui/material/styles';
 import { ThemeProvider } from '@emotion/react';
+import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 import App from './App';
@@ -10,10 +14,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <ThemeProviderMui theme={createTheme()}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </ThemeProviderMui>
     </React.StrictMode>
   </BrowserRouter>
 );
